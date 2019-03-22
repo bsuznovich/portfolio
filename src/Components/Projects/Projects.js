c5a4a074d3c74039f1a6246d1bad000926ae693b
Brody Suznovich <brodysuznovich@yahoo.com>
2019-03-22 13:34:43 -0600

    skills page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,20 +1,36 @@
 import React from 'react'
+import './Projects.scss'
 
 export default function Projects(){
     return(
-        <div>
-            <h2>Projects</h2>
-            <h3>Sorting Place</h3>
-            <p>This is a Harry Potter themed website. Here, the user is able to enroll (once done, the user will receive an acceptance email), be sorted into a school house, edit profile (change name, house points, and add a photo), and see information about each house.</p>
-            <p>This web application is built with React. Other technologies include: redux (to hold user information), nodemailer (to email the user and acceptance letter), and  amazon S3 (to allow the user to upload pictures).</p>
-            <p>GITHUB</p>
-            <p>Sorting Place</p>
+        <div className='projects'>
+            <div className='projectHolder'>
+                <h2>Projects</h2>
 
-            <h3>Cramify</h3>
-            <p>This is a Kahoot like website. Here, the user can sign up, set up a study based game and play with their friends. They also have the ability to create and delete their own question sets. The user can also log in as a guest to play. </p>
-            <p>This web application is built with React. Other technologies include: redux, sockets, and SASS.</p>
-            <p>GITHUB</p>
-            <p>Cramify</p>
+                <div className='sortingPlace'>
+                    <h3>Sorting Place</h3>
+                    <p>This is a Harry Potter themed website. Here, the user is able to enroll (once done, the user will receive an acceptance email), be sorted into a school house, edit profile (change name, house points, and add a photo), and see information about each house.</p>
+                    <p>This web application is built with React. Other technologies include: redux (to hold user information), nodemailer (to email the user and acceptance letter), and  amazon S3 (to allow the user to upload pictures).</p>
+                    <a href='https://github.com/bsuznovich/schoolofhogwarts' target='_blank'>
+                        <p>GITHUB</p>
+                    </a>
+                    <a href='http://www.sortingplace.xyz/#/' target='_blank'>
+                        <p>Sorting Place</p>
+                    </a>
+                </div>
+
+                <div className='cramify'>
+                    <h3>Cramify</h3>
+                    <p>This is a Kahoot like website. Here, the user can sign up, set up a study based game and play with their friends. They also have the ability to create and delete their own question sets. The user can also log in as a guest to play. </p>
+                    <p>This web application is built with React. Other technologies include: redux, sockets, and SASS.</p>
+                    <a href='https://github.com/Cramify/cramify' target='_blank'>
+                        <p>GITHUB</p>
+                    </a>
+                    <a href='https://cramify.net/#/' target='_blank'>
+                        <p>Cramify</p>
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -4,20 +4,34 @@ import './Skills.scss'
 export default function Skills(){
     return(
         <div className='skills'>
-            <div className='skillHolder'>
+            <div className='container'>
                 <h2>Skills</h2>
-                <img src='' alt='' />
-                <h5>HTML</h5>
-                <img src='' alt='' />
-                <h5>React</h5>
-                <img src='' alt='' />
-                <h5>JavaScript</h5>
-                <img src='' alt='' />
-                <h5>CSS</h5>
-                <img src='' alt='' />
-                <h5>SQL</h5>
-                <img src='' alt='' />
-                <h5>Node</h5>
+                <div className='skillsHolder'>
+                    <div className='html'>
+                        <img src='https://cdn0.iconfinder.com/data/icons/HTML5/512/HTML_Logo.png' alt='' />
+                        <h5>HTML</h5>
+                    <div className='css'>
+                        <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/CSS.3.svg/730px-CSS.3.svg.png' alt='' />
+                        <h5>CSS</h5>
+                    </div>
+                    </div>
+                    <div className='react'>
+                        <img src='https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png' alt='' />
+                        <h5>React</h5>
+                    <div className='js'>
+                        <img src='https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png' alt='' />
+                        <h5>JavaScript</h5>
+                    </div>
+                    </div>
+                    <div className='sql'>
+                        <img src='http://www.stickpng.com/assets/images/584815fdcef1014c0b5e497a.png' alt='' />
+                        <h5>SQL</h5>
+                    <div className='node'>
+                        <img src='https://nodejs.org/static/images/logos/nodejs-new-pantone-black.png' alt='' />
+                        <h5>Node</h5>
+                    </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
